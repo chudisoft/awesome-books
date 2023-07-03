acefb530183b90a57316738e98615c90d35b9d82
christopheruduekwe@gmail.com <christopheruduekwe@gmail.com>
2023-07-03 20:47:54 +0100

    Fix webhint error

--- assets/script.js
+++ assets/script.js
@@ -1,25 +1,25 @@
 let bookList = [];
-const form = document.querySelector("form");
-const bookListElement = document.querySelector("#book-list");
-const bookStorageName = "books";
+const form = document.querySelector('form');
+const bookListElement = document.querySelector('#book-list');
+const bookStorageName = 'books';
 // add book function
 function addBook(title, author) {
   const book = { title, author };
-  const divBook = document.createElement("div");
-  divBook.className = "book";
-  const titleHeader = document.createElement("h5");
-  const authorHeader = document.createElement("h5");
-  const hr = document.createElement("hr");
-  title.className = "title";
-  author.className = "author";
+  const divBook = document.createElement('div');
+  divBook.className = 'book';
+  const titleHeader = document.createElement('h5');
+  const authorHeader = document.createElement('h5');
+  const hr = document.createElement('hr');
+  title.className = 'title';
+  author.className = 'author';
   titleHeader.textContent = book.title;
   authorHeader.textContent = book.author;
-  const removeBtn = document.createElement("button");
-  removeBtn.textContent = "Remove";
+  const removeBtn = document.createElement('button');
+  removeBtn.textContent = 'Remove';
   removeBtn.book = book; //  store the book object as a property of the remove button
-  removeBtn.addEventListener("click", () => {
+  removeBtn.addEventListener('click', () => {
     bookList = bookList.filter(
-      (_book) => _book.title !== book.title || _book.author !== book.author
+      (_book) => _book.title !== book.title || _book.author !== book.author,
     );
     localStorage.setItem(bookStorageName, JSON.stringify(bookList));
     bookListElement.removeChild(divBook);
@@ -33,7 +33,7 @@ function addBook(title, author) {
 //  refuctor booklist
 function renderBookList() {
   const storedvalue = localStorage.getItem(bookStorageName);
-  bookListElement.textContent = "";
+  bookListElement.textContent = '';
   bookList = [];
   if (storedvalue !== null) {
     bookList = JSON.parse(storedvalue);
@@ -44,16 +44,16 @@ function renderBookList() {
 }
 renderBookList();
 //  add an event listener to the form submit button
-form.addEventListener("submit", (event) => {
+form.addEventListener('submit', (event) => {
   event.preventDefault();
-  const title = document.querySelector("#title").value.trim();
-  const author = document.querySelector("#author").value.trim();
-  if (title !== "" && author !== "") {
+  const title = document.querySelector('#title').value.trim();
+  const author = document.querySelector('#author').value.trim();
+  if (title !== '' && author !== '') {
     const book = { title, author };
     addBook(title, author);
     bookList.push(book);
     localStorage.setItem(bookStorageName, JSON.stringify(bookList));
-    document.querySelector("#title").value = "";
-    document.querySelector("#author").value = "";
+    document.querySelector('#title').value = '';
+    document.querySelector('#author').value = '';
   }
 });
