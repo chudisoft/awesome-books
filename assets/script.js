8472e5e071d7df82b1fad0f96240a3b97fb7f63d
christopheruduekwe@gmail.com <christopheruduekwe@gmail.com>
2023-07-04 10:19:44 +0100

    Add styles for the book list. Use javascript to color books.

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,4 @@
+let counter = 0;
 let bookList = [];
 class Book {
   constructor(title, author) {
@@ -5,41 +6,43 @@ class Book {
     this.author = author;
   }
 }
-const form = document.querySelector("form");
-const bookListElement = document.querySelector("#book-list");
-const bookStorageName = "books";
+const form = document.querySelector('form');
+const bookListElement = document.querySelector('#book-list');
+const bookStorageName = 'books';
 // add book function
 function addBook(title, author) {
+  counter += 1;
   const book = new Book(title, author);
-  const divBook = document.createElement("div");
-  divBook.className = "book";
-  const titleHeader = document.createElement("h5");
-  const authorHeader = document.createElement("h5");
-  const hr = document.createElement("hr");
-  titleHeader.className = "title";
-  authorHeader.className = "author";
-  titleHeader.textContent = book.title;
-  authorHeader.textContent = book.author;
-  const removeBtn = document.createElement("button");
-  removeBtn.textContent = "Remove";
+  const divBook = document.createElement('div');
+  if (counter % 2 === 1) { divBook.className = 'book'; } else { divBook.className = 'book bg-white'; }
+  const titleHeader = document.createElement('label');
+  titleHeader.className = 'title';
+  titleHeader.textContent = `"${book.title}" by ${book.author}`;
+  const removeBtn = document.createElement('button');
+  removeBtn.textContent = 'Remove';
+  removeBtn.className = 'btn btn-remove';
   removeBtn.book = book; //  store the book object as a property of the remove button
-  removeBtn.addEventListener("click", () => {
+  removeBtn.addEventListener('click', () => {
     bookList = bookList.filter(
-      (_book) => _book.title !== book.title || _book.author !== book.author
+      (_book) => _book.title !== book.title || _book.author !== book.author,
     );
     localStorage.setItem(bookStorageName, JSON.stringify(bookList));
     bookListElement.removeChild(divBook);
+    // Recolor
+    counter = 0;
+    bookListElement.childNodes.forEach((element) => {
+      counter += 1;
+      if (counter % 2 === 1) { element.className = 'book'; } else { element.className = 'book bg-white'; }
+    });
   });
   divBook.appendChild(titleHeader);
-  divBook.appendChild(authorHeader);
-  divBook.appendChild(hr);
   divBook.appendChild(removeBtn);
   bookListElement.appendChild(divBook);
 }
 //  refuctor booklist
 function renderBookList() {
   const storedvalue = localStorage.getItem(bookStorageName);
-  bookListElement.textContent = "";
+  bookListElement.textContent = '';
   bookList = [];
   if (storedvalue !== null) {
     bookList = JSON.parse(storedvalue);
@@ -50,16 +53,16 @@ function renderBookList() {
 }
 renderBookList();
 //  add an event listener to the form submit button
-form.addEventListener("submit", (event) => {
+form.addEventListener('submit', (event) => {
   event.preventDefault();
-  const title = document.querySelector("#title").value.trim();
-  const author = document.querySelector("#author").value.trim();
-  if (title !== "" && author !== "") {
+  const title = document.querySelector('#title').value.trim();
+  const author = document.querySelector('#author').value.trim();
+  if (title !== '' && author !== '') {
     const book = new Book(title, author);
     addBook(title, author);
     bookList.push(book);
     localStorage.setItem(bookStorageName, JSON.stringify(bookList));
-    document.querySelector("#title").value = "";
-    document.querySelector("#author").value = "";
+    document.querySelector('#title').value = '';
+    document.querySelector('#author').value = '';
   }
 });
